20676d9bfd4b885a8006dfacf064548072f98005
Sergey Novikov <synopticum@gmail.com>
2026-08-19 13:29:17 +0300

    Добавить базовый каркас

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Canvas } from '@react-three/fiber';
+
+export default function App() {
+    return (
+        <Canvas>
+            <color attach="background" args={['#fffaf0']} />
+        </Canvas>
+    );
+}
