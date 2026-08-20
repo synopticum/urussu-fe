47c1c1c6969ab8b6f7dff1352326a52d2992cc3b
Sergey Novikov <synopticum@gmail.com>
2026-08-20 15:22:42 +0300

    Поправить MAX_ZOOM

--- src/components/rtf-app/map-camera/constants.ts
+++ src/components/rtf-app/map-camera/constants.ts
@@ -1,5 +1,5 @@
 export const PADDING = 0.98;
 export const MIN_ZOOM = 1.5;
-export const MAX_ZOOM = 8;
+export const MAX_ZOOM = 6;
 export const ZOOM_SPEED = 1.0045;
 export const DEFAULT_ZOOM = 2.5;
